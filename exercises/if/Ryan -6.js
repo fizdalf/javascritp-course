b1832b8ab54c42acfa9cb58b67e745056c26ed1e
Ryan <tvmuixi@hotmail.com>
2022-11-07 18:18:22 +0000

    ejercicio 6

---
+++ exercises/if/Ryan -6.js
@@ -0,0 +1,36 @@
+switch (process.argv[2]) {
+    case "0" :
+        data = "zero";
+        break;
+    case "1" :
+        data = "one";
+        break;
+    case "2" :
+        data = "two";
+        break;
+    case "3" :
+        data = "three";
+        break;
+    case "4" :
+        data = "four";
+        break;
+    case "5" :
+        data = "five";
+        break;
+    case "6" :
+        data = "six";
+        break;
+    case "7" :
+        data = "seven";
+        break;
+    case "8" :
+        data = "eight";
+        break;
+    case "9" :
+        data = "nine";
+        break;
+    default:
+        data = "invalid number";
+        break;
+}
+console.log(data);
