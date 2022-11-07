d600ceae7af7d322160ce440e9a1594f27cc0187
kevin <kevinlopezfreire@hotmail.com>
2022-11-07 19:21:31 +0100

    exercise 7

---
+++ exercises/if/exercise 7.js
@@ -0,0 +1,18 @@
+let cantidadOvejas = parseInt(process.argv[2]);
+let ovejas = 1;
+
+while (ovejas <= cantidadOvejas) {
+    console.log(ovejas + " Ovejas");
+    ovejas++;
+
+
+
+    if (ovejas === 1) {
+        console.log(ovejas + " Oveja");
+
+
+    }
+
+}
+
+
