0bd8a01b958a7871f0177b90a8d60ee6afe53cb9
kevin <kevinlopezfreire@hotmail.com>
2022-11-07 19:20:41 +0100

    exercise 6

---
+++ exercises/if/exercise 6.js
@@ -0,0 +1,43 @@
+const userNumber = process.argv[2];
+switch (userNumber) {
+    case '1':
+        console.log("uno");
+        break;
+    case '2':
+        console.log("dos");
+
+        break;
+    case '3':
+        console.log("tres");
+
+        break;
+    case '4':
+        console.log("cuatro");
+
+        break;
+    case '5':
+        console.log("cinco");
+
+        break;
+    case '6':
+        console.log("seis");
+
+        break;
+    case '7':
+        console.log("siete");
+
+        break;
+    case '8':
+        console.log("ocho");
+
+        break;
+    case '9':
+        console.log("nueve");
+        break;
+    case '0':
+        console.log("cero");
+        break;
+    default:
+        console.log("");
+        break;
+}
