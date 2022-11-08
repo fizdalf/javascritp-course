c1147e8d05e7a0f314c7594a108b4002869af294
Ryan <tvmuixi@hotmail.com>
2022-11-08 20:37:13 +0000

    8

--- exercises/loops/ryan-8.js
+++ exercises/loops/ryan-8.js
@@ -2,5 +2,12 @@ let piramide = parseInt(process.argv[2]);
 let message = "";
 while (piramide === 1) {
     message = "*";
+
+    if (piramide === 2) {
+        message = `*\n* *`;
+    }
+    if (piramide === 3) {
+        message = `*\n* *\n* * *`;
+    }
 }
 console.log(message);
