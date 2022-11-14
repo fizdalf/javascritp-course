5203eb42e207d53169bc08159f5a8716096fdf1b
Ryan <tvmuixi@hotmail.com>
2022-11-14 17:52:58 +0000

    8

--- exercises/loops/ryan-8.1.js
+++ exercises/loops/ryan-8.1.js
@@ -1,5 +1,4 @@
 let levels = parseInt(process.argv[2]);
-let lastLevelAsteriscs = (levels * 2) - 1;
 let asterisc = `*`;
 let space = ` `;
 let number = 1;
@@ -7,16 +6,20 @@ let firstAsterisc = 1;
 let message1 = "";
 let message2 = "";
 let message3 = "";
+let levelsMessage1 = `${levels - 1}`;
 
-while (lastLevelAsterisc >= number) {
-    message1 = `${space * (levels - 1)}`;
+while (levelsMessage1 >= number) {
+    message1 = space;
+}
+levelsMessage1 = levelsMessage1 - 1;
+
+while (levels >= number) {
+    message1 = `${space * (levelsMessage1)}`;
     message2 = `${asterisc * firstAsterisc}`;
-    message3 = `${space * (levels -1)}`;
-
-    console.log(message1+message2+message3);
-
+    message3 = `${space * (levelsMessage1)}`;
+    console.log(message1 + message2 + message3);
     firstAsterisc = + 2;
-    lastLevelAsteriscs = lastLevelAsteriscs - 1;
+    levels = levels - 1;
 }
 console.log("Fin de la Piramidaca");
 
