382becf4d42a50b19fed7cc239f1dbb8cab1fe6d
Ryan <tvmuixi@hotmail.com>
2022-11-20 21:07:01 +0000

    8.2

--- exercises/loops/ryan-8.2.exerciceshomework14.js
+++ exercises/loops/ryan-8.2.exerciceshomework14.js
@@ -46,5 +46,62 @@ console.log(circleValues(c));
 // 6. Write a function to reverse a number
 // Function `reverseNum()` returns the reversed number for the given argument number value.
 function reverseNum(a) {
-    return a
+    let reverse = 0;
+    while(a != 0)
+    {
+        reverse = reverse * 10;
+        reverse = reverse + a%10;
+        a = Math.trunc(a/10); // Remove decimal places
+    }
+    return reverse;
 }
+console.log(reverseNum(a));
+
+// 7. Count number of Vowels in String
+// Function `countVowel()` returns the number of vowels in input string. Learn more about JavaScript String methods
+// from javatpoint.com/javascript-string.
+let str = process.argv[5];
+function countVowel(str) {
+    var count = 0;
+   str=str.toLowerCase(); // estó hacé que todo sea en minúsculas
+    for (let i = 0; i <str.length;i++) {
+        if (str.charAt(i)=="a"||str.charAt(i)=="e"||str.charAt(i)=="i"||str.charAt(i)=="o"||str.charAt(i)=="u"){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowel(str));
+
+// 8. Flatten array of arrays using JavaScript reduce
+// Function `flattenArr()` flattens a 2D array by combining each sub array into 1D array by using JavaScript reduce.
+function flattenArr(arr) {
+    return arr.reduce((result, array) => result.concat(array));
+}
+console.log(flattenArr([[1,2,3],[4,5,6],[7,8,9]]));
+// no se pk no me va
+
+//9. Write a function to check if an input string is a palindrome
+// Function `checkPalindrome()` return a boolean value based on whether the input string is palindrome or not.
+function checkPalindrome(str) {
+    for (let i = 0; i < str.length; i++) {
+        if(str.charAt(i) != str.charAt(str.length-i-1)) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(checkPalindrome(str));
+
+// 10. Write a function to calculate simple interest based on the principle amount
+// Function `simpleInt()` returns a final amount based on the simple interest formula provided principal amount,
+// rate of interest per year, and time on a yearly basis.
+
+
+// I ask a small girl how old is she.
+var x = "";
+function age () {
+    var x = rand(x) % 10; // Genera un valor entre 0 y 9
+    return `${x} years old`;
+}
+console.log(age);
