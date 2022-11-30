755eae16f474074931d361b9a2f28b78b7f632ee
Ryan <tvmuixi@hotmail.com>
2022-11-30 13:58:26 +0000

    9. palindromo

--- exercises/contactmentor.com/js-function-exercises-solution/ryan-9.js
+++ exercises/contactmentor.com/js-function-exercises-solution/ryan-9.js
@@ -1,6 +1,14 @@
 //9. Write a function to check if an input string is a palindrome
 // Function `checkPalindrome()` return a boolean value based on whether the input string is palindrome or not.
 function checkPalindrome(word) {
+    for (let i = 0; i < word.length; i++) {
+        if (word.charAt(i) != word.charAt(word.length - i - 1)) {
+            return false;
+        }
+    }
+    return true
+}
+
     if (word === `a`) {
         return true
     }
@@ -16,14 +24,18 @@ function checkPalindrome(word) {
     if (word === `abb`) {
         return false
     }
+    if (word === `abba`) {
+        return true
+    }
 }
-console.log(checkPalindrome(`abb`));
+console.log(checkPalindrome(`abmba`));
 
 console.assert(checkPalindrome(`a`) === true, "should return true when given aa");
 console.assert(checkPalindrome(`aa`) === true, "should return true when given aa");
 console.assert(checkPalindrome(`ab`) === false, "should return false when given ab");
 console.assert(checkPalindrome(`aba`) === true, "should return true when given aba");
 console.assert(checkPalindrome(`abb`) === false, "should return false when given abb");
+console.assert(checkPalindrome(`abba`) === true, "should return true when given abba");
 
 
 
