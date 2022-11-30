02c002fbe7b3fcb7cfa92e00f70eb91376ad8198
Ryan <tvmuixi@hotmail.com>
2022-11-30 14:11:25 +0000

    10. je ne se pa

--- exercises/contactmentor.com/js-function-exercises-solution/ryan-10.js
+++ exercises/contactmentor.com/js-function-exercises-solution/ryan-10.js
@@ -1,3 +1,10 @@
 // 10. Write a function to calculate simple interest based on the principle amount
 // Function `simpleInt()` returns a final amount based on the simple interest formula provided principal amount,
 // rate of interest per year, and time on a yearly basis.
+function simpleInt(amount, interest, basis) {
+    let finalAmount = amount + amount*interest*basis;
+    return finalAmount;
+}
+
+console.assert(finalAmount(20000,5,2)) === 220000,
+console.assert(finalAmount(150000,25,1)) === 3900000
