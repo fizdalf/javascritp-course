650829f0a63e3b4a7d5d9a2b874c08af9456f877
Ryan <tvmuixi@hotmail.com>
2022-12-04 20:56:47 +0000

    count

---
+++ exercises/codewars/ryan-count.js
@@ -0,0 +1,9 @@
+// Create a function args_count, that returns the count of passed arguments
+function args_count (a,b,c) {
+   // tengo un string
+    cuento quantos objetos hay en el string
+    doy el numero de objetos del string
+}
+
+console.assert(args_count(1, 2, 3) === 3, "args_count should return 3 when given (1, 2, 3)");
+console.assert(args_count(1, 2, 3, 10) === 4, "args_count should return 4 when given (1, 2, 3, 10)");
