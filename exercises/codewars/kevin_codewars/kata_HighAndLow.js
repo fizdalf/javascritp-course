97d6d64a464e32060794e9de9bc59014a0b3ffdb
kevin <kevinlopezfreire@hotmail.com>
2022-12-07 19:35:46 +0100

    challenges

---
+++ exercises/codewars/kevin_codewars/kata_HighAndLow.js
@@ -0,0 +1,8 @@
+function highAndLow(numbers) {
+    numbers = toString(numbers).split(' ');
+    console.log(Math.max(Number(numbers)),
+            console.log(Math.min(Number(numbers));
+
+}
+
+highAndLow("10 5 6 -6 3");
