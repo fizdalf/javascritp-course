3c14f0f102381c9a487cd9d844c0734b2c996e6b
fizdalf <fizdalf@gmail.com>
2022-12-12 18:47:43 +0000

    refactor

--- exercises/codewars/highest_and_lowest/highest_and_lowest.js
+++ exercises/codewars/highest_and_lowest/highest_and_lowest.js
@@ -1,12 +1,4 @@
 module.exports = function (numberString) {
     const numbers = numberString.split(" ");
-
-    if (numbers.length === 3) {
-        return `${numbers[0]} ${numbers[0]}`;
-    }
-
-    if (numbers.length === 2) {
-        return numberString;
-    }
-    return `${numberString} ${numberString}`;
+    return `${numbers[0]} ${numbers[0]}`;
 };
