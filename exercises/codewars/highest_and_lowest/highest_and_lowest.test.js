f97ef6af72c864492644a13885705fc3a52d8461
fizdalf <fizdalf@gmail.com>
2022-12-12 18:11:01 +0000

    created function

--- exercises/codewars/highest_and_lowest/highest_and_lowest.test.js
+++ exercises/codewars/highest_and_lowest/highest_and_lowest.test.js
@@ -1,3 +1,4 @@
+const highAndLow = require('./highest_and_lowest');
 describe('highAndLow', () => {
     it('should return "1 1" when given "1"', () => {
         expect(highAndLow("1")).toBe("1 1");
