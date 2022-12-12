332d192031f462a1ae2ed9f7b90b4087ac59ab19
fizdalf <fizdalf@gmail.com>
2022-12-12 19:00:34 +0000

    refactor

--- exercises/codewars/highest_and_lowest/highest_and_lowest.js
+++ exercises/codewars/highest_and_lowest/highest_and_lowest.js
@@ -1,17 +1,4 @@
 module.exports = function (numberString) {
-
-
-    if (numberString === "25 -90") {
-        return "25 -90";
-    }
-
-    if (numberString === "3 -9") {
-        return "3 -9";
-    }
-
-    if (numberString === "2 1") {
-        return "2 1";
-    }
     const numbers = numberString.split(" ");
     let max = numbers[0];
     let min = numbers[numbers.length < 2 ? 0 : 1];
