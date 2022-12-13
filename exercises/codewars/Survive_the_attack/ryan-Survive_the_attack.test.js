4dbe1da186ff60fef89d2307638e705e5eac4147
Ryan <tvmuixi@hotmail.com>
2022-12-13 14:28:14 +0000

    test 2 passed

--- exercises/codewars/Survive_the_attack/ryan-Survive_the_attack.test.js
+++ exercises/codewars/Survive_the_attack/ryan-Survive_the_attack.test.js
@@ -4,4 +4,7 @@ describe(`surviveTheAttack`, () => {
     it('should return "true" when given "[] []"', () => {
         expect(surviveTheAttack([], [])).toBe("true");
     });
+    it('should return "false" when given "[1] [1]"', () => {
+        expect(surviveTheAttack([1], [1])).toBe("false");
+    });
 })
