918b44a423351243d2b08754d21098ffe527df74
Ryan <tvmuixi@hotmail.com>
2022-12-13 11:28:43 +0000

    survive the attack

--- exercises/codewars/ryan-nextpalindromicnumber.js
+++ exercises/codewars/ryan-nextpalindromicnumber.js
@@ -1,29 +1,18 @@
 // make a function named nexPal() that gives the next palindromic number bigger than the number given.
-function isAPalindromic(num) {
-    for (let i = 0; i < number.length / 2; i++) {
-        if (num.charAt(i) != num.charAt(num.length - i - 1)){
-            return false;
-        }
-    }
-    return true;
-}
-let firstNum = process.argv[2];
-let num = firstNum;
-let theNum = "";
-function nexPal(num) {
-    for (let i = num; i >= num; i++) {
-        let testNum = + 1;
-        if (isAPalindromic(testNum)) {
-            let testNum = theNum;
-        } else {
-            nexPal()
-        }
-    }
+function nextPal(val) {
+  let palindromicNum = val++
+
+   }
+
 }
 
+function palindromicNum(num) {
+    const numToString = String(num);
+    return numToString.split("").reverse().join("") === numToString;
 
 
-console.assert(nexPal(1) === 11, "nextPal should return 11 when given 1");
+}
+
 console.assert(nexPal(11) === 22, "nextPal should return 22 when given 11");
 
 
