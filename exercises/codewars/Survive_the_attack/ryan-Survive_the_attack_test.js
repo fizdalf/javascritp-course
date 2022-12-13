d07d701853150e3336cb7046247b4cd7290df266
Ryan <tvmuixi@hotmail.com>
2022-12-13 11:43:25 +0000

    preparación básica archives

--- exercises/codewars/Survive_the_attack/ryan-Survive_the_attack_test.js
+++ exercises/codewars/Survive_the_attack/ryan-Survive_the_attack_test.js
@@ -1,5 +1,4 @@
 const surviveTheAttack = require('./ryan-Survive_The_Attack');
-const nextPal = require("../next-palindromic-number/next-palindromic-number");
 
 describe(`surviveTheAttack`, () => {
     it.each([
