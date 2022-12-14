70c1acbc3f5b08a135b353e35e6a1ba06893c2db
fizdalf <fizdalf@gmail.com>
2022-12-14 19:33:23 +0000

    refactor

--- projects/arrimate/next-movement.js
+++ projects/arrimate/next-movement.js
@@ -6,14 +6,11 @@ function nextMovement(board, player, line, moves) {
             const dots = [];
             return [[".", "x", ...dots, "y"]];
         }
-    }
-    if (board.length === 1) {
         if (board[0].length === 4) {
             const dots = ["."];
             return [[".", "x", ...dots, "y"]];
         }
-    }
-    if (board.length === 1) {
+
         if (board[0].length === 5) {
             const dots = [".", "."];
             return [[".", "x", ...dots, "y"]];
