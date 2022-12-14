f33b83ddfb7f516200b15d1feb4af27cadaf567f
Ryan <tvmuixi@hotmail.com>
2022-12-14 18:04:15 +0000

    copy

--- exercises/codewars/Survive_the_attack/ryan-Survive_the_attack.test.js
+++ exercises/codewars/Survive_the_attack/ryan-Survive_the_attack.test.js
@@ -1,10 +1,13 @@
-const surviveTheAttack = require('./ryan-Survive_The_Attack');
+const hasSurvived = require('./ryan-Survive_The_Attack');
 
-describe(`surviveTheAttack`, () => {
+describe(`hasSurvived`, () => {
     it('should return "true" when given "[] []"', () => {
-        expect(surviveTheAttack([], [])).toBe("true");
+        expect(hasSurvived([], [])).toBe("true");
     });
-    it('should return "false" when given "[1] [1]"', () => {
-        expect(surviveTheAttack([1], [1])).toBe("false");
-    });
+  //  it('should return "false" when given "[1] [1]"', () => {
+  //      expect(surviveTheAttack([1], [1])).toBe("false");
+  //  });
+  //  it('should return "false" when given "[2] [2]"', () => {
+ //       expect(surviveTheAttack([1], [1])).toBe("false");
+  //  });
 })
