a5e66696b1a9bd32d7995c69561e950aa3543469
fizdalf <fizdalf@gmail.com>
2022-12-19 19:32:11 +0000

    refactor

--- projects/arrimate/countDots.js
+++ projects/arrimate/countDots.js
@@ -3,19 +3,21 @@ module.exports = countDots;
 function countDots(line) { // [".", "x", ".", ".", "y"]
     let dotsAfterX = 0;
     if (line.length === 4) {
+        let dotsAfterX = 2;
         return {
             dotsBeforeX: 0,
-            dotsAfterX: 2
+            dotsAfterX: dotsAfterX
         };
     }
     if (line.length === 3) {
+        let dotsAfterX = 1;
         return {
             dotsBeforeX: 0,
-            dotsAfterX: 1
+            dotsAfterX: dotsAfterX
         };
     }
     return {
         dotsBeforeX: 0,
-        dotsAfterX: 0
+        dotsAfterX: dotsAfterX
     };
 }
