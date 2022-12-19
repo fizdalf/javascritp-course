c71d7332082e15d1ac61ada95a510452155444f2
fizdalf <fizdalf@gmail.com>
2022-12-19 18:29:24 +0000

    use giveMeDots even more

--- projects/arrimate/next-movement.js
+++ projects/arrimate/next-movement.js
@@ -13,22 +13,23 @@ function nextMovement(board, player, line, moves) {
     if (board[0].length > 2) {
 
         if (board[0][1] === 'x') {
-            let dotsBeforeX =  giveMeDots(2);
-            return [[...dotsBeforeX, "x", ".", ".", "y"]]
+            let dotsAfterX = giveMeDots(2)
+            let dotsBeforeX = giveMeDots(2);
+            return [[...dotsBeforeX, "x", ...dotsAfterX, "y"]];
+        } else if (board[0][2] === 'x') {
+            let dotsAfterX = giveMeDots(1);
+            let dotsBeforeX = giveMeDots(3);
+            return [[...dotsBeforeX, "x", ...dotsAfterX, "y"]];
+        } else if (board[0][3] === 'x') {
+            let dotsAfterX = giveMeDots(0);
+            let dotsBeforeX = giveMeDots(4);
+            return [[...dotsBeforeX, "x", ...dotsAfterX, "y"]];
+        } else {
+            let numberOfDotsWeNeed = board[0].length - 3;
+            let dotsAfterX = giveMeDots(numberOfDotsWeNeed);
+            let dotsBeforeX = giveMeDots(1);
+            return [[...dotsBeforeX, "x", ...dotsAfterX, "y"]];
         }
-        if (board[0][2] === 'x') {
-            let dotsBeforeX =  giveMeDots(3);
-            return [[...dotsBeforeX, "x", ".", "y"]]
-        }
-        if (board[0][3] === 'x') {
-            let dotsAfterX=giveMeDots(0)
-            let dotsBeforeX =  giveMeDots(4);
-            return [[...dotsBeforeX, "x", ...dotsAfterX, "y"]]
-        }
-        let numberOfDotsWeNeed = board[0].length - 3;
-        let dotsAfterX = giveMeDots(numberOfDotsWeNeed);
-        let dotsBeforeX = giveMeDots(1);
-        return [[...dotsBeforeX, "x", ...dotsAfterX, "y"]];
     }
     throw "invalid movement";
 }
