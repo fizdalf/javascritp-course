6bfb8c24bd9d8581c2e1f75f97e6636f655a0f9e
fizdalf <fizdalf@gmail.com>
2022-12-19 19:26:31 +0000

    making code look similar

--- projects/arrimate/countDots.test.js
+++ projects/arrimate/countDots.test.js
@@ -6,5 +6,7 @@ describe('countDots', function () {
     it('should return an object with the one in dotsAfterX when given an array with a dot after x', () => {
         expect(countDots(["x",".","y"])).toStrictEqual({dotsBeforeX: 0, dotsAfterX: 1});
     });
-
+    it('should return an object with the two in dotsAfterX when given an array with two dots after x', () => {
+        expect(countDots(["x",".","y"])).toStrictEqual({dotsBeforeX: 0, dotsAfterX: 1});
+    });
 });
