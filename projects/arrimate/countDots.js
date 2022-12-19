1ef6b7c69a66156fb766f234be64e7490537c993
fizdalf <fizdalf@gmail.com>
2022-12-19 19:33:35 +0000

    creating test for countDots

--- projects/arrimate/countDots.js
+++ projects/arrimate/countDots.js
@@ -1,13 +1,9 @@
 module.exports = countDots;
 
 function countDots(line) { // [".", "x", ".", ".", "y"]
-    let dotsAfterX = 0;
+    let dotsAfterX =0;
     if (line.length === 4) {
-        let dotsAfterX = 2;
-        return {
-            dotsBeforeX: 0,
-            dotsAfterX: dotsAfterX
-        };
+       dotsAfterX = 2;
     }
     if (line.length === 3) {
         let dotsAfterX = 1;
