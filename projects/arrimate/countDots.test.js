865bf55f88fd8c96b48538d82c06fbad7aaeb1d8
fizdalf <fizdalf@gmail.com>
2022-12-21 18:36:23 +0000

    new test!

--- projects/arrimate/countDots.test.js
+++ projects/arrimate/countDots.test.js
@@ -9,22 +9,28 @@ describe('countDots', function () {
     it('should return an object with the two in dotsAfterX when given an array with two dots after x', () => {
         expect(countDots(["x", ".", ".", "y"])).toStrictEqual({dotsBeforeX: 0, dotsAfterX: 2});
     });
-    it('should return an object with the two in dotsAfterX when given an array with two dots after x', () => {
+    it('should return an object with 0 dotsAfterX and 1 dotsBeforeX when given an array with one dot before x', () => {
         expect(countDots([".", "x", "y"])).toStrictEqual({
             dotsBeforeX: 1,
             dotsAfterX: 0
         });
     });
-    it('should return an object with the two in dotsAfterX when given an array with two dots after x', () => {
+    it('should return an object with 0 dotsAfterX and 2 dotsBeforeX when given an array with two dots before x', () => {
         expect(countDots([".", ".", "x", "y"])).toStrictEqual({
             dotsBeforeX: 2,
             dotsAfterX: 0
         });
     });
-    it('should return an object with the two in dotsAfterX when given an array with two dots after x', () => {
+    it('should return an object with 0 in dotsAfterX and 3 in dotsBeforeX when given an array with three dots before x', () => {
         expect(countDots([".", ".", ".", "x", "y"])).toStrictEqual({
             dotsBeforeX: 3,
             dotsAfterX: 0
+        });
+    });
+    it('should return an object with 1 dotsAfterX and 1 dotBeforeX when given an array with 1 dot after x and 1 dot before x', () => {
+        expect(countDots([".", "x", ".", "y"])).toStrictEqual({
+            dotsBeforeX: 1,
+            dotsAfterX: 1
         });
     });
 });
