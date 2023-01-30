3ad4e1f54271222e01fb176ab2ab33db3b8ae177
fizdalf <fizdalf@gmail.com>
2023-01-30 18:21:16 +0000

    fix tests

--- projects/arrimate/canMoveInLine.test.js
+++ projects/arrimate/canMoveInLine.test.js
@@ -4,11 +4,11 @@ describe('canMoveInLine', function () {
     it('should return true if one dot is between x and y', () => {
         let board = [["x", ".", "y"]];
         let line = 1;
-        expect(canMoveInLine(board, line)).toStrictEqual("true");
+        expect(canMoveInLine(board, line)).toStrictEqual(true);
     });
     it('should return false if zero dots are between x and y', () => {
         let board = [["x", "y"]];
         let line = 1;
-        expect(canMoveInLine(board, line)).toStrictEqual("false");
+        expect(canMoveInLine(board, line)).toStrictEqual(false);
     });
 });
