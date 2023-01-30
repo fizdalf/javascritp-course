b8c8a9e3baf3243699256b8c32cfd0ff4061ef64
fizdalf <fizdalf@gmail.com>
2023-01-30 19:22:02 +0000

    add exercise

--- projects/arrimate/testInquirer.js
+++ projects/arrimate/testInquirer.js
@@ -1,21 +1,48 @@
 import inquirer from 'inquirer';
 
-inquirer
-    .prompt([
-        {
-            type: 'confirm',
-            name: 'toBeDelivered',
-            message: 'Is this for delivery?',
-            default: false,
-        },
-    ])
-    .then((answers) => {
-        console.log(answers);
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.log(error)
-        } else {
-            console.log(error)
-        }
-    });
+
+const main = async () => {
+    let selectedOption = null;
+    do {
+        const answer = await inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'options',
+                    message: 'Please select one of the options',
+                    choices: [
+                        {
+                            name: 'Print Board',
+                            value: 'printBoard',
+                        },
+                        {
+                            name: 'Exit',
+                            value: 'exit',
+                        },
+                    ],
+                },
+            ]);
+        selectedOption = answer.options
+
+        // TODO: create a function stringifyBoard(board) that will print the table to the console with the following structure
+        //  | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+        //  +---+---+---+---+---+---+---+
+        //  | . | X | . | . | Y | . | . | <- 1
+        //  +---+---+---+---+---+---+---+
+        //  | X | . | . | . | . | . | Y | <- 2
+        //  +---+---+---+---+---+---+---+
+        //  | X | . | . | . | . | . | Y | <- 3
+        //  +---+---+---+---+---+---+---+
+        //  Use the function to show the board when the user selects the "Print Board" option
+        //  Use TDD to create the function that will return a STRING representation of the board, this function will be used
+        //  by printBoard to print the board to the console
+        const exampleBoard = [
+            [".", "X", ".", ".", "Y", ".", "."],
+            ["X", ".", ".", ".", ".", ".", "Y"],
+            ["X", ".", ".", ".", ".", ".", "Y"],
+        ]
+        printBoard(exampleBoard) // this function prints the board.
+    } while (selectedOption !== 'exit');
+}
+
+main();
