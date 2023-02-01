adf9f30b92c6b118474ba919cd84d8ef2380c1d8
fizdalf <fizdalf@gmail.com>
2023-02-01 18:56:00 +0000

    Changes by Antonio Mejías R

--- projects/arrimate/stringify-board.test.js
+++ projects/arrimate/stringify-board.test.js
@@ -8,6 +8,14 @@ describe('stringifyBoard', function () {
         ]
         expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 |\n+---+\n| . | <- 1\n+---+\n");
     });
+    it('should print a string version of a board with just one line, and one cell containing a x', () => {
+        const boardWithJustOneCell = [
+            [
+                "x"
+            ]
+        ]
+        expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 |\n+---+\n| x | <- 1\n+---+\n");
+    });
 });
 
 
