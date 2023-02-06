241cc242c01f87efba5c2f50b1ab15c06fd93a3f
Ryan <tvmuixi@hotmail.com>
2023-02-06 09:34:53 +0000

    empiezo de 0 again

--- projects/arrimate/stringify-board.js
+++ projects/arrimate/stringify-board.js
@@ -5,6 +5,7 @@ let lineNumber = " <- 1";
 let rightStick = " |";
 let middleStick = " | ";
 
+
 function getHeader(elements) {
     let header = leftStick + 1;
     for (let i = 2; i <= elements; i++) {
@@ -22,23 +23,26 @@ function getSpacing(elements) {
     return spacing + "\n";
 }
 
-function getLine(board) {
-    let line = leftStick + board[0][0];
-    for (let i = 0; i <= board[0].length ; i++) {
-        line += middleStick + board[0][i];
-    }
-    return line + rightStick + lineNumber + "\n";
-}
-
 function stringifyBoard(board) {
 
+    let line = leftStick + board[0][0];
+    line = line + rightStick + lineNumber + "\n";
 
+    if (board[0].length >= 2) {
+        line = leftStick + board[0][0] + middleStick + board[0][1] + rightStick + lineNumber + "\n";
+    }
+    if (board[0].length >= 3) {
+        line = leftStick + board[0][0] + middleStick + board[0][1] + middleStick + board[0][2] + rightStick + lineNumber + "\n";
+    }
+    if (board[0].length >= 4) {
+        line = leftStick + board[0][0] + middleStick + board[0][1] + middleStick + board[0][2] + middleStick + board[0][3] + rightStick + lineNumber + "\n";
+    }
     let header = getHeader(board[0].length);
     let spacing = getSpacing(board[0].length);
-    let line = getLine(board[0].length);
 
     return header + spacing + line + spacing;
 
 }
+
 
 module.exports = stringifyBoard;
--- projects/arrimate/stringify-board.test.js
+++ projects/arrimate/stringify-board.test.js
@@ -1,52 +1,52 @@
 const stringifyBoard = require('./stringify-board.js');
 describe('stringifyBoard', function () {
     it('should print a string version of a board with just one line, and one cell containing a space', () => {
-        const boardWithJustOneCell = [
+        const board = [
             [
                 "."
             ]
         ]
-        expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 |\n+---+\n| . | <- 1\n+---+\n");
+        expect(stringifyBoard(board)).toStrictEqual("| 1 |\n+---+\n| . | <- 1\n+---+\n");
     });
     it('should print a string version of a board with just one line, and one cell containing a x', () => {
-        const boardWithJustOneCell = [
+        const board = [
             [
                 "x"
             ]
         ]
-        expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 |\n+---+\n| x | <- 1\n+---+\n");
+        expect(stringifyBoard(board)).toStrictEqual("| 1 |\n+---+\n| x | <- 1\n+---+\n");
     });
     it('should print a string version of a board with just one line, and one cell containing a y', () => {
-        const boardWithJustOneCell = [
+        const board = [
             [
                 "y"
             ]
         ]
-        expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 |\n+---+\n| y | <- 1\n+---+\n");
+        expect(stringifyBoard(board)).toStrictEqual("| 1 |\n+---+\n| y | <- 1\n+---+\n");
     });
     it('should print a string version of a board with just one line, and two cell containing one a x and one a y', () => {
-        const boardWithJustOneCell = [
+        const board = [
             [
                 "x", "y"
             ]
         ]
-        expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 | 2 |\n+---+---+\n| x | y | <- 1\n+---+---+\n");
+        expect(stringifyBoard(board)).toStrictEqual("| 1 | 2 |\n+---+---+\n| x | y | <- 1\n+---+---+\n");
     });
     it('should print a string version of a board with just one line, and three cells containing X, . and Y', () => {
-        const boardWithJustOneCell = [
+        const board = [
             [
                 "x", ".", "y"
             ]
         ]
-        expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 | 2 | 3 |\n+---+---+---+\n| x | . | y | <- 1\n+---+---+---+\n");
+        expect(stringifyBoard(board)).toStrictEqual("| 1 | 2 | 3 |\n+---+---+---+\n| x | . | y | <- 1\n+---+---+---+\n");
     });
     it('should print a string version of a board with just one line, and four cells containing X, ., . and Y', () => {
-        const boardWithJustOneCell = [
+        const board = [
             [
                 "x", ".", ".", "y"
             ]
         ]
-        expect(stringifyBoard(boardWithJustOneCell)).toStrictEqual("| 1 | 2 | 3 | 4 |\n+---+---+---+---+\n| x | . | . | y | <- 1\n+---+---+---+---+\n");
+        expect(stringifyBoard(board)).toStrictEqual("| 1 | 2 | 3 | 4 |\n+---+---+---+---+\n| x | . | . | y | <- 1\n+---+---+---+---+\n");
     });
 });
 
