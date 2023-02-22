48cfbee938248a271f26b45ae04e2fcab57bfc96
fizdalf <fizdalf@gmail.com>
2023-02-22 19:25:02 +0000

    finished game

--- projects/arrimate/arrimate.js
+++ projects/arrimate/arrimate.js
@@ -23,18 +23,6 @@ async function getDimension() {
     return dimension;
 }
 
-async function askStep() {
-    return await inquirer.prompt(
-        [
-            {
-                type: 'number',
-                name: 'steps',
-                message: 'Player X, how many steps to move in line 1',
-            }
-        ]
-    );
-}
-
 function isValidMovement(lineNumber, board, steps) {
     let lineIndex = lineNumber - 1;
     const line = board[lineIndex];
@@ -88,16 +76,4 @@ function printBoard(board) {
 }
 
 main();
-
-// TODO: yo quiero un programa a traves de consola, k me pregunta, de cuanto quiero el tablero?
-// dos numeros separados por comas, min 1X1 sino error message, max 10x10 (k jilipollez) pero error si no cumplen.
-// mostrar tablero en estado inicial. acabar partidas por linias. primero la 1 luego la 2.
-// primero juega x luego y.
-// pedir linia y num de espacios (if not possible error) and try again.
-// diseño en paint xD word... recordar lo de los menus que empezamos en inquirer
-// al final indicar el ganador
-//
-// gadgets: poner que si ponen q en segun que sitio exit program.
-// si se piran, end game
-
 
