de5c35b234bfcc66230184abc3c51c781d2ea71f
fizdalf <fizdalf@gmail.com>
2023-03-13 19:58:46 +0000

    Added challenge

---
+++ exercises/codewars/maze-runner/maze-runner.ts
@@ -0,0 +1,22 @@
+export enum MazeRoom {
+    StartingPoint = 2,
+    Wall = 1,
+    FinishPoint = 3,
+}
+
+export enum MazeResult {
+    Dead = "Dead"
+}
+
+export function mazeRunner(maze: number[][], directions: string[]): string {
+    if (maze.length >= 2) {
+        if (directions[0] === "E" || directions[0] === "S" ) {
+            return "Dead";
+        }
+        return "Finish";
+    }
+    if (directions.length > 0) {
+        return "Dead";
+    }
+    return "Lost";
+}
