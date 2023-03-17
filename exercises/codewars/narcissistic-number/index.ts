f13ec1c01fc72ad52b9bbabb1c7e1a6a0c4c216f
fizdalf <fizdalf@gmail.com>
2023-03-17 19:44:42 +0000

    Added challenge

--- exercises/codewars/narcissistic-number/index.ts
+++ exercises/codewars/narcissistic-number/index.ts
@@ -1,26 +1,19 @@
-export function isNarcissistic(value: number): boolean {
-    if (value === 10 || value === 11 || value === 12 || value === 13) {
-        return false;
-    }
-
-    return true;
-    // if(calculateNarcissus(value) === value){
-    //     return true;
-    // }
-    // return false;
-
+export function isNarcissistic(givenNumber: number): boolean {
+    return calculateNarcissus(givenNumber) === givenNumber
 }
 
-export function calculateNarcissus(value: number): number {
-    const digitsCount = value.toString().length;
-
-    const lastDigit = value % 10;
-    let result = lastDigit ** digitsCount;
-    if (value >= 10 && value <= 99) {
-        const remainingNumber = Math.floor(value / 10);
-        const secondDigit = remainingNumber ** digitsCount;
-        result += secondDigit;
-    }
+export function calculateNarcissus(givenNumber: number) {
+    let result = 0;
+    let numberOfDigits = givenNumber.toString().length;
+    let remainder = givenNumber;
+    do {
+        let lastDigit = remainder % 10;
+        remainder = Math.floor(remainder / 10);
+        result += lastDigit ** numberOfDigits;
+    } while (remainder > 0)
 
     return result;
 }
+
+
+/// 1581 % 10 -> 1 ---- 10 / 10 -> math.floor(158.1) -> 158 -> 158 % 10 -> 8 ---  mf(158/10) -> 15 // 15 % 10 -> 5 | mf(15/10) -> 1 // mf(1/10) -> 0
--- exercises/codewars/narcissistic-number/narcissistic-number.test.ts
+++ exercises/codewars/narcissistic-number/narcissistic-number.test.ts
@@ -4,26 +4,14 @@ describe('isNarcissistic', function () {
     it('should return true when given 1', () => {
         expect(isNarcissistic(1)).toBe(true);
     });
-    it('should return true when given 2', () => {
-        expect(isNarcissistic(2)).toBe(true);
-    });
-    it('should return true when given 3', () => {
-        expect(isNarcissistic(3)).toBe(true);
-    });
     it('should return false when given 10', () => {
         expect(isNarcissistic(10)).toBe(false);
     });
-
-    it('should return false when given 11', () => {
-        expect(isNarcissistic(11)).toBe(false);
+    it('should return true when given 153', () => {
+        expect(isNarcissistic(153)).toBe(true);
     });
-
-    it('should return false when given 12', () => {
-        expect(isNarcissistic(12)).toBe(false);
+    it('should return true when given 1634', () => {
+        expect(isNarcissistic(1634)).toBe(true);
     });
-    it('should return false when given 13', () => {
-        expect(isNarcissistic(13)).toBe(false);
-    });
-
-
 });
+
