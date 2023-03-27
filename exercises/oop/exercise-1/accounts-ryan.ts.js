0af3569722983e641b46042e568ffa8ac2d3446d
Ryan <tvmuixi@hotmail.com>
2023-03-27 12:04:03 +0100

    starting account

--- exercises/oop/exercise-1/accounts-ryan.ts.js
+++ exercises/oop/exercise-1/accounts-ryan.ts.js
@@ -1,4 +1,49 @@
+export class account {
+    private _accountNumber: number;
+    private _balance: number;
 
-class Account (balance: number, accountNumber: number) {
-    withdraw
+    constructor(z: number) {
+        this._accountNumber = z;
+        this._balance = 0;
+    }
+
+    public withdraw(sum: number) {
+        if (sum>0)
+            this._balance -= sum;
+        else
+            console.log('Account withdraw error');
+    }
+    public deposit(sum: number) {
+        if (sum>0) {
+            this._balance += sum;
+        else
+            console.log('Account deposit error');
+        }
+    }
+    get accountNumber(): number {
+        return this._accountNumber;
+    }
+    get balance(): number {
+        return this._balance;
+    }
+
 }
+
+export class SavingAccount {
+    private _account: number;
+    private _interest: number;
+
+    constructor(y: number) {
+        this._account = y;
+        this._interest = 0;
+    }
+
+    public addInterest(sum: number) {
+        if (sum>0) {
+            this._interest += sum;
+        }
+    }
+
+}
+
+
