3c5e84afbebcbb2b067b3c4a66a5b8e6732433e0
fizdalf <fizdalf@gmail.com>
2023-04-12 19:02:04 +0100

    one formula

--- exercises/codewars/narcissistic-number/PriceOfMangoes.ts
+++ exercises/codewars/narcissistic-number/PriceOfMangoes.ts
@@ -1,6 +1,6 @@
 export function mango(quantity: number, price: number): number {
-    let lastPrice = quantity - Math.floor(quantity / 3);
-    return lastPrice *= price
+    const lastPrice = quantity - Math.floor(quantity / 3);
+    return lastPrice * price;
 
 }
 
--- exercises/codewars/narcissistic-number/QuarterOfTheYear.ts
+++ exercises/codewars/narcissistic-number/QuarterOfTheYear.ts
@@ -1,17 +1,3 @@
 export function quarterOf(month: number): any {
-
-    if (month <= 3) {
-        return 1
-    }
-
-    if (month <= 6) {
-        return 2
-    }
-    if (month <= 9) {
-        return 3
-    }
-    if (month <= 12) {
-        return 4
-    }
-
+    return Math.ceil(month * 4 / 12);
 }
