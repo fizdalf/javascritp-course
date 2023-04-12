f97f56300e7a26b28095c2538b7dacf6ca5d9763
Ryan <tvmuixi@hotmail.com>
2023-04-12 18:12:57 +0100

    all fatal

---
+++ exercises/codewars/Ryan/Filter out the geese/filterOutOfTheGeese.test.ts
@@ -0,0 +1,12 @@
+import {filterOutOfTheGeese} from './filterOutOfTheGeese';
+
+describe('filterOutOfTheGeese', ()=> {
+    it('should get an array and get back the same array', function () {
+        expect(filterOutOfTheGeese( ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]))
+    .toStrictEqual(["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
+    });
+    it('should get an array and take out the words from the geese, and return the array without them', function () {
+        expect(filterOutOfTheGeese(["Mallard", "African", "Hook Bill", "Crested", "Blue Swedish"]))
+            .toStrictEqual(["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
+    });
+})
---
+++ exercises/codewars/Ryan/Filter out the geese/filterOutOfTheGeese.ts
@@ -0,0 +1,4 @@
+export function filterOutOfTheGeese (array: string[]): string[] {
+    const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return array.filter(( z ) => ![geese].includes(z))};
+
