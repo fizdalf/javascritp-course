5ef93be5eb2a6d49c59180f093f9c8b83b064a45
fizdalf <fizdalf@gmail.com>
2023-04-12 19:46:40 +0100

    improve code

--- exercises/codewars/narcissistic-number/fillter.ts
+++ exercises/codewars/narcissistic-number/fillter.ts
@@ -1,5 +1,19 @@
-
 const unwantedGeese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-export function gooseFilter(birds:any): any {
-    return birds.filter((goose: string) => !unwantedGeese.includes(goose));
+
+function isUnwantedGoose(goose: string): boolean {
+    for (let i = 0; i < unwantedGeese.length ; i++) {
+        const unwantedGoose = unwantedGeese[i];
+        if(unwantedGoose === goose){
+            return  true;
+        }
+    }
+    return false;
+}
+
+
+export function gooseFilter(birds: any): any {
+    return birds.filter((goose: string) => {
+        let b = isUnwantedGoose(goose);
+        return !b;
+    });
 }
