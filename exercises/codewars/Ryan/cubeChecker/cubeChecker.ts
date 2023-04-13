84b6909988554a5381490984faab8737348c064d
Ryan <tvmuixi@hotmail.com>
2023-04-14 00:10:43 +0100

    4 test Refactor cutre salxixero

--- exercises/codewars/Ryan/cubeChecker/cubeChecker.ts
+++ exercises/codewars/Ryan/cubeChecker/cubeChecker.ts
@@ -1,11 +1,5 @@
 export function cubeChecker(volume: number, side: number) :boolean {
-    if (volume === 451) {
-        return false;
-    }
-    if (volume === 0) {
-        return false;
-    }
-    if (volume === 2) {
+    if (volume === 451 || volume === 0 || volume === 2) {
         return false;
     }
     return true;
