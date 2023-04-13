18fcda7f386c26ebeebf7728deef441728b5404c
Ryan <tvmuixi@hotmail.com>
2023-04-14 00:08:12 +0100

    4 test bueno pk falla

--- exercises/codewars/Ryan/cubeChecker/cubeChecker.test.ts
+++ exercises/codewars/Ryan/cubeChecker/cubeChecker.test.ts
@@ -9,4 +9,7 @@ describe('cubeChecker', function () {
     it('should return false when volume 0 and side 35', function () {
         expect(cubeChecker(0,35)).toBe(false);
     });
+    it('should return false when volume 451 and side 1', function () {
+        expect(cubeChecker(451,1)).toBe(false);
+    });
 });
