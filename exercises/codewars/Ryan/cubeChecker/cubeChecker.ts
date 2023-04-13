869cfc736269fef948a8368472733ba90f0e161a
Ryan <tvmuixi@hotmail.com>
2023-04-14 00:13:46 +0100

    super refactor

--- exercises/codewars/Ryan/cubeChecker/cubeChecker.ts
+++ exercises/codewars/Ryan/cubeChecker/cubeChecker.ts
@@ -1,6 +1,6 @@
-export function cubeChecker(volume: number, side: number) :boolean {
-    if (volume === 451 || volume === 0 || volume === 2) {
-        return false;
+export function cubeChecker(volume: number, side: number): boolean {
+    if (volume === side * side * side) {
+        return true;
     }
-    return true;
+    return false;
 }
