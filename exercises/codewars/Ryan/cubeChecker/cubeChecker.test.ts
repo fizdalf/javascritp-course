85bcbe66c21f1756e4370da8e89366bab4549b21
Ryan <tvmuixi@hotmail.com>
2023-04-14 00:04:27 +0100

    3er test malo pk pasa de xiripa

--- exercises/codewars/Ryan/cubeChecker/cubeChecker.test.ts
+++ exercises/codewars/Ryan/cubeChecker/cubeChecker.test.ts
@@ -6,4 +6,7 @@ describe('cubeChecker', function () {
     it('should return false when volume 2 and side 1', function () {
         expect(cubeChecker(2,1)).toBe(false);
     });
+    it('should return true when volume 8 and side 2', function () {
+        expect(cubeChecker(8,2)).toBe(true);
+    });
 });
