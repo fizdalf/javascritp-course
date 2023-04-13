cfb95ca1740b5fa7dce06427741f40b938193def
Ryan <tvmuixi@hotmail.com>
2023-04-13 23:13:52 +0100

    unversioned files

--- exercises/codewars/Ryan/Filter out the geese/filterOutOfTheGeese.ts
+++ exercises/codewars/Ryan/Filter out the geese/filterOutOfTheGeese.ts
@@ -1,4 +1,6 @@
 export function filterOutOfTheGeese (array: string[]): string[] {
     const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    return array.filter(( z ) => ![geese].includes(z))};
-
+    return array.filter((z) => {
+        ![geese].includes(z)
+    });
+};
