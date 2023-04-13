90387d91a0e35ba2016222d68f7fad018a95c226
Ryan <tvmuixi@hotmail.com>
2023-04-14 00:05:55 +0100

    3er test reformado bueno pk falla

--- exercises/codewars/Ryan/cubeChecker/cubeChecker.test.ts
+++ exercises/codewars/Ryan/cubeChecker/cubeChecker.test.ts
@@ -6,7 +6,7 @@ describe('cubeChecker', function () {
     it('should return false when volume 2 and side 1', function () {
         expect(cubeChecker(2,1)).toBe(false);
     });
-    it('should return true when volume 8 and side 2', function () {
-        expect(cubeChecker(8,2)).toBe(true);
+    it('should return false when volume 0 and side 35', function () {
+        expect(cubeChecker(0,35)).toBe(false);
     });
 });
