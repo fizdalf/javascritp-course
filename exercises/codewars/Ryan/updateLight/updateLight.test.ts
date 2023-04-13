13a14d5f4f36d33e6a695ca2b5c2c977a1d83f7a
Ryan <tvmuixi@hotmail.com>
2023-04-14 00:34:05 +0100

    second test down

--- exercises/codewars/Ryan/updateLight/updateLight.test.ts
+++ exercises/codewars/Ryan/updateLight/updateLight.test.ts
@@ -4,4 +4,7 @@ describe('updateLight', function () {
     it('should return green when given red', function () {
         expect(updateLight('red')).toStrictEqual('green');
     });
+    it('should return yellow when given green', function () {
+        expect(updateLight('green')).toStrictEqual('yellow');
+    });
 })
