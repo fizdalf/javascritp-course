18f344598e57befba954a902c127d7cf2d58d665
Ryan <tvmuixi@hotmail.com>
2023-04-14 14:55:00 +0100

    4to test pasando bien

--- exercises/codewars/Ryan/bmi/bmi.ts
+++ exercises/codewars/Ryan/bmi/bmi.ts
@@ -1,11 +1,13 @@
-export function bmi(wight: number, height: number):string {
-   let bmiX = wight/(height*height);
+export function bmi(wight: number, height: number): string {
+    let bmiX = wight / (height * height);
+    if (bmiX > 30) {
+        return 'Obese';
+    }
     if (bmiX <= 30 && bmiX > 25) {
         return 'Overweight';
     }
-   if (bmiX <= 25 && bmiX > 18.5) {
-       return 'Normal';
-   }
-
+    if (bmiX <= 25 && bmiX > 18.5) {
+        return 'Normal';
+    }
     return 'Underweight';
 }
