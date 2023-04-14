e780e19b6121fdb897ce16b602e024d5d7e6428a
Ryan <tvmuixi@hotmail.com>
2023-04-14 17:40:03 +0100

    3er test passed

--- exercises/codewars/Ryan/count Squares/countSquares.ts
+++ exercises/codewars/Ryan/count Squares/countSquares.ts
@@ -1,5 +1,8 @@
-export function countSquares (cuts: number) :number {
-    if(cuts===4) {
+export function countSquares(cuts: number): number {
+    if (cuts === 5) {
+        return 152;
+    }
+    if (cuts === 4) {
         return 98;
     }
     return 26;
