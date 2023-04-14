3c41efcbe88d8c32a6059898c4476ddaf0680c86
Ryan <tvmuixi@hotmail.com>
2023-04-14 14:39:40 +0100

    2o test falla bien

--- exercises/codewars/Ryan/bmi/bmi.test.ts
+++ exercises/codewars/Ryan/bmi/bmi.test.ts
@@ -4,4 +4,7 @@ describe('bmi', function () {
     it('should return "Underweight" if bmi <= 18.5', function () {
         expect(bmi(60, 1.8)).toBe('Underweight');
     });
+    it('should return "Normal" if bmi <= 25', function () {
+        expect(bmi(60, 1.8)).toBe('Normal');
+    });
 });
