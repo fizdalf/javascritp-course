8ffdb2a45659b94748277d77b3032fdba8c540a6
Ryan <tvmuixi@hotmail.com>
2023-04-14 14:47:25 +0100

    2o test passa, fallo pk puse || en lugar de &&. solved

--- exercises/codewars/Ryan/bmi/bmi.test.ts
+++ exercises/codewars/Ryan/bmi/bmi.test.ts
@@ -2,7 +2,7 @@ import {bmi} from "./bmi";
 
 describe('bmi', function () {
     it('should return "Underweight" if bmi <= 18.5', function () {
-        expect(bmi(60, 1.8)).toBe('Underweight');
+        expect(bmi(20, 1.8)).toBe('Underweight');
     });
     it('should return "Normal" if bmi <= 25', function () {
         expect(bmi(60, 1.8)).toBe('Normal');
--- exercises/codewars/Ryan/bmi/bmi.ts
+++ exercises/codewars/Ryan/bmi/bmi.ts
@@ -1,3 +1,8 @@
 export function bmi(wight: number, height: number):string {
+   let bmiX = wight/(height*height);
+    if (bmiX <= 25 && bmiX > 18.5) {
+       return 'Normal';
+   }
+
     return 'Underweight';
 }
