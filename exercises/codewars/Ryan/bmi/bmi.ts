f5b17c92a35330d18bebd561be74fea00b9afa67
Ryan <tvmuixi@hotmail.com>
2023-04-14 14:51:23 +0100

    3er test pasa

--- exercises/codewars/Ryan/bmi/bmi.ts
+++ exercises/codewars/Ryan/bmi/bmi.ts
@@ -1,6 +1,9 @@
 export function bmi(wight: number, height: number):string {
    let bmiX = wight/(height*height);
-    if (bmiX <= 25 && bmiX > 18.5) {
+    if (bmiX <= 30 && bmiX > 25) {
+        return 'Overweight';
+    }
+   if (bmiX <= 25 && bmiX > 18.5) {
        return 'Normal';
    }
 
