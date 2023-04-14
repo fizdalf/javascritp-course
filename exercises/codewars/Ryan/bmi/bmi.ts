ef7f4ea9393da2f1cb803d56c1186bd3829dab25
Ryan <tvmuixi@hotmail.com>
2023-04-14 15:16:33 +0100

    refactor final copiando de la peña.

--- exercises/codewars/Ryan/bmi/bmi.ts
+++ exercises/codewars/Ryan/bmi/bmi.ts
@@ -1,16 +1,4 @@
-export function bmi(wight: number, height: number): string {
-    let bmiX = wight / (height * height);
-    if (bmiX > 30) {
-        return 'Obese';
-    }
-    if (bmiX <= 30 && bmiX > 25) {
-        return 'Overweight';
-    }
-    if (bmiX <= 25 && bmiX > 18.5) {
-        return 'Normal';
-    }
-    if (bmiX < 18.5) {
-        return 'Underweight';
-    }
-    throw new Error("The method or operation is not implemented.");
-}
+export function bmi(weight: number, height: number): string {
+    const bmiX = weight / (height * height);
+    return bmiX <= 18.5 ? 'Underweight' : bmiX <= 25.0 ? 'Normal' : bmiX <= 30.0 ? 'Overweight' : 'Obese';
+};
