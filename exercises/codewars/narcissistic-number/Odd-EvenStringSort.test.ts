2829fb8911b0261a6db6a13d8415cbb28070e5f7
kevin <kevinlopezfreire@hotmail.com>
2023-04-18 13:07:30 +0200

    challengesº

--- exercises/codewars/narcissistic-number/Odd-EvenStringSort.test.ts
+++ exercises/codewars/narcissistic-number/Odd-EvenStringSort.test.ts
@@ -1,9 +1,12 @@
 import {sortMyString} from "./Odd-EvenStringSort";
 describe('sortMyString', function (){
-    it('should ', function () {
+    it('should return s when given s ', function () {
         expect(sortMyString('s')).toBe('s')
     });
-    it('should ', function () {
+    it('should return is when given si', function () {
         expect(sortMyString('si')).toBe('is')
+    });
+    it('should return ois when given sio ', function () {
+        expect(sortMyString('sio')).toBe('ois')
     });
 })
--- exercises/codewars/narcissistic-number/Odd-EvenStringSort.ts
+++ exercises/codewars/narcissistic-number/Odd-EvenStringSort.ts
@@ -2,6 +2,9 @@ export function sortMyString(S: string): string {
     if ( S === 'si'){
         return 'is'
     }
+    if ( S === 'sio'){
+        return 'ois'
+    }
 
     return 's' ;
 }
